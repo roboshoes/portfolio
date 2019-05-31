a2f3659ced9dc11921a392b4cc752866e5f5ad7a
Mathias Paumgarten <mail@mathias-paumgarten.com>
2019-05-31 11:43:16 -0700

    adding two sections

--- main.tsx
+++ main.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { render } from "react-dom";
+
+import { Brag } from "./scripts/components/brag";
 import { Header } from "./scripts/components/header";
+import { Who } from "./scripts/components/who";
 
 const App: React.FunctionComponent = () => (
-    <Header />
+    <>
+        <Header />
+        <Who />
+        <Brag />
+    </>
 );
 
 
--- scripts/components/header.tsx
+++ scripts/components/header.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 import styles from "./header.scss";
 
 export const Header: React.FunctionComponent = () => (
-    <>
+    <header className={ styles.header }>
         <span className={ styles.bar }></span>
         <span className={ styles.title }>MATHIAS PAUMGARTEN</span>
-    </>
+    </header>
 );

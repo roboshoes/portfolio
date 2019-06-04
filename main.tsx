5ce3b6ddfd678d21a2e6cc00a80f744d4e2b8df4
Mathias Paumgarten <mail@mathias-paumgarten.com>
2019-06-03 17:11:09 -0700

    adds mouse moventment

--- main.tsx
+++ main.tsx
@@ -5,11 +5,13 @@ import { Brag } from "./scripts/components/brag";
 import { Header } from "./scripts/components/header";
 import { Who } from "./scripts/components/who";
 import { Contact } from "./scripts/components/contact";
+import { Work } from "./scripts/components/work";
 
 const App: React.FunctionComponent = () => (
     <>
         <Header />
         <Who />
+        <Work />
         <Brag />
         <Contact />
     </>
--- scripts/components/work.tsx
+++ scripts/components/work.tsx
@@ -1,15 +1,76 @@
 import * as React from "react";
+import { bindAll } from "lodash";
 
 import sharedStyles from "../../styles/shared.scss";
+import styles from "./work.scss";
 
-export const Work: React.FunctionComponent = () => (
-    <div>
-        <div className={ sharedStyles.titleWrapper }>
-            <div className={ sharedStyles.bar }></div>
-            <div className={ sharedStyles.title }>WORK</div>
-        </div>
-        <div className={ sharedStyles.paragraph }>
-            01 - 02 - 03 - Window Wonderland - 05
-        </div>
-    </div>
-);
+
+export class Work extends React.Component {
+
+    private latestY = 0;
+    private mouseY = 0;
+    private raf = -1;
+
+    state = {
+        anchor: 15
+    };
+
+    constructor( props: {} ) {
+        super( props );
+
+        bindAll( this, "onMouseDown", "onMouseMove", "onMouseUp", "loop" );
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={ sharedStyles.titleWrapper }>
+                    <div className={ sharedStyles.bar }></div>
+                    <div className={ sharedStyles.title }>WORK</div>
+                </div>
+                <div className={ sharedStyles.paragraph }>
+                    01 - 02 - 03 - Window Wonderland - 05
+                </div>
+                <div className={ styles.container } onMouseDown={ e => this.onMouseDown( e ) }>
+                    <div className={ styles.block } style={ { transform: `translateX( ${ this.state.anchor }px )` } }>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    private loop() {
+        cancelAnimationFrame( this.raf );
+
+        const delta = this.mouseY - this.latestY;
+
+        this.setState( { anchor: this.state.anchor + delta } );
+
+        this.raf = requestAnimationFrame( this.loop );
+        this.latestY = this.mouseY;
+    }
+
+    private onMouseDown( event: React.MouseEvent<HTMLDivElement, MouseEvent> ) {
+        this.mouseY = event.pageX;
+        this.latestY = event.pageX;
+
+        window.addEventListener( "mousemove", this.onMouseMove );
+        window.addEventListener( "mouseup", this.onMouseUp );
+
+        this.loop();
+    }
+
+    private onMouseMove( event: MouseEvent ) {
+        this.mouseY = event.pageX;
+    }
+
+    private onMouseUp( event: MouseEvent ) {
+        window.removeEventListener( "mousemove", this.onMouseMove );
+        window.removeEventListener( "mouseup", this.onMouseUp );
+
+        this.latestY = 0;
+
+        cancelAnimationFrame( this.raf );
+    }
+}

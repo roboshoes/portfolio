f7d8486889647f5c783afd5f88bb554f8746ec41
Mathias Paumgarten <mail@mathias-paumgarten.com>
2019-07-02 14:00:52 -0700

    updates text animate in

--- scripts/components/text.tsx
+++ scripts/components/text.tsx
@@ -1,5 +1,6 @@
 import classnames from "classnames";
-import { Power2, Power3, SlowMo, TimelineMax } from "gsap";
+import { Power2, Power3, TimelineMax } from "gsap";
+import { random, times } from "lodash";
 import * as React from "react";
 
 import ss from "../../styles/shared.scss";
@@ -9,28 +10,21 @@ interface TextProps {
     text: string;
 }
 
-interface Point {
-    x: number;
-    y: number;
+interface Line {
+    begin: number;
+    end: number;
 }
 
 export class Text extends React.Component<TextProps> {
 
     private parentRef: React.RefObject<HTMLDivElement>;
     private canvasRef: React.RefObject<HTMLCanvasElement>;
-    private whiteAnchors: Point[] = [];
-    private darkAnchors: Point[] = [];
 
     constructor( props: TextProps ) {
         super( props );
 
         this.parentRef = React.createRef<HTMLDivElement>();
         this.canvasRef = React.createRef<HTMLCanvasElement>();
-
-        for ( let i = 0; i < 4; i++ ) {
-            this.whiteAnchors.push( { x: 0, y: 0 } );
-            this.darkAnchors.push( { x: 0, y: 0 } );
-        }
     }
 
     componentDidMount() {
@@ -39,24 +33,25 @@ export class Text extends React.Component<TextProps> {
             const context = canvas.getContext( "2d" )!;
             const size = this.parentRef.current.getBoundingClientRect();
             const backgroundColor = "#0F0F0F";
+            const foregroundColor = "white";
 
             const width = canvas.width = size.width;
             const height = canvas.height = size.height;
 
-            this.darkAnchors[ 1 ] = { x: width, y: 0 };
-            this.darkAnchors[ 2 ] = { x: width, y: height };
-            this.darkAnchors[ 3 ] = { x: 0, y: height };
-
-            context.fillStyle = "white";
-            context.fillRect( 0, 0, canvas.width, canvas.height );
-
-            const timeline: TimelineMax = this.createTimeline( width, height );
+            const amount = height / 24;
+            const lines: Line[] = times( amount, () => ( { begin: 0, end: 0 }  as Line ) );
+            const timeline = this.createLinesTimeline( lines, width );
 
             timeline.eventCallback( "onUpdate", () => {
                 context.clearRect( 0, 0, width, height );
 
-                this.renderQuad( context, this.darkAnchors, backgroundColor );
-                this.renderQuad( context, this.whiteAnchors );
+                lines.forEach( ( line: Line, i: number ) => {
+                    context.fillStyle = backgroundColor;
+                    context.fillRect( line.end, i * 24 + 1, width - line.end, 21 );
+
+                    context.fillStyle = foregroundColor;
+                    context.fillRect( line.begin, i * 24 + 1, line.end - line.begin, 21 );
+                } );
             } );
 
             timeline.play();
@@ -70,31 +65,15 @@ export class Text extends React.Component<TextProps> {
         </div>;
     }
 
-    private createTimeline( width: number, height: number ) {
+    private createLinesTimeline( lines: Line[], width: number ): TimelineMax {
         const timeline = new TimelineMax( { paused: true, autoRemoveChildren: true } );
 
-        timeline.to( this.whiteAnchors[ 1 ], 1.1, { x: width, ease: SlowMo.ease.config( 0.3, 0.3, false ) } );
-        timeline.to( this.whiteAnchors[ 2 ], 0.3, { y: height, ease: Power2.easeOut }, 0.1 );
-        timeline.to( this.whiteAnchors[ 3 ], 0.3, { y: height, ease: Power2.easeOut }, 0.1 );
-        timeline.to( this.whiteAnchors[ 2 ], 0.7, { x: width, ease: Power2.easeInOut }, 0.41 );
-
-        timeline.to( this.whiteAnchors[ 0 ], 0.5, { x: width, ease: Power3.easeInOut }, 0.9 );
-        timeline.to( this.whiteAnchors[ 3 ], 0.6, { x: width, ease: Power3.easeInOut }, 0.96 );
-
-        timeline.to( this.darkAnchors[ 0 ], 0.5, { x: width, ease: Power3.easeInOut }, 0.9 );
-        timeline.to( this.darkAnchors[ 3 ], 0.6, { x: width, ease: Power3.easeInOut }, 0.96 );
+        lines.forEach( ( line: Line, i: number ) => {
+            const offset = random( 0.2, 0.4, true );
+            timeline.to( line, 0.9, { end: width, ease: Power3.easeOut }, i * 0.05 + offset );
+            timeline.to( line, 0.8, { begin: width, ease: Power2.easeInOut }, 0.2 + i * 0.03 + offset );
+        } );
 
         return timeline;
-    }
-
-    private renderQuad( context: CanvasRenderingContext2D, quad: Point[], color = "white" ) {
-        context.beginPath();
-        context.moveTo( quad[ 0 ].x, quad[ 0 ].y );
-        context.lineTo( quad[ 1 ].x, quad[ 1 ].y );
-        context.lineTo( quad[ 2 ].x, quad[ 2 ].y );
-        context.lineTo( quad[ 3 ].x, quad[ 3 ].y );
-        context.closePath();
-        context.fillStyle = color;
-        context.fill();
     }
 }

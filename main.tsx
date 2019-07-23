372ebe2d2b1deef7d7cc8a3df26a4fccdba135c3
Mathias Paumgarten <mail@mathias-paumgarten.com>
2019-07-23 14:53:29 -0700

    scrolls to top on detail view

--- main.tsx
+++ main.tsx
@@ -5,23 +5,45 @@ import * as Webfont from "webfontloader";
 import { Background } from "./scripts/components/background";
 import { Brag } from "./scripts/components/brag";
 import { Contact } from "./scripts/components/contact";
-import { Detail } from "./scripts/components/detail";
+import { Detail, DETAIL_ROUTE } from "./scripts/components/detail";
 import { Header } from "./scripts/components/header";
 import { Who } from "./scripts/components/who";
 import { Work } from "./scripts/components/work";
+import { observeRoute } from "./scripts/services/router";
 import ss from "./styles/shared.scss";
 
-const App: React.FunctionComponent = () => <>
-    <Background />
-    <div className={ ss.site }>
-        <Header delay={ 200 } />
-        <Who delay={ 400 } />
-        <Work delay={ 600 } />
-        <Brag delay={ 1000 }/>
-        <Contact delay={ 1200 }/>
-        <Detail />
-    </div>
-</>;
+class App extends React.Component {
+
+    private siteRef = React.createRef<HTMLDivElement>();
+
+    componentDidMount() {
+        if ( this.siteRef.current ) {
+            observeRoute( DETAIL_ROUTE ).subscribe( ( on: boolean ) => {
+                if ( on ) {
+                    this.siteRef.current!.scrollTo( {
+                        top: 0,
+                        behavior: "smooth"
+                    } );
+                }
+            } );
+        }
+    }
+
+    render() {
+        return <>
+            <Background />
+            <div className={ ss.site } ref={ this.siteRef }>
+                <Header delay={ 200 } />
+                <Who delay={ 400 } />
+                <Work delay={ 600 } />
+                <Brag delay={ 1000 }/>
+                <Contact delay={ 1200 }/>
+                <Detail />
+            </div>
+        </>;
+    }
+}
+
 
 Webfont.load( {
     active: () => render( <App/>, document.getElementById( "root" ) ),

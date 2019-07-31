57a45f853c8e163bfb90f8e05fde4cca250e8ce0
Mathias Paumgarten <mail@mathias-paumgarten.com>
2019-07-30 22:32:10 -0700

    mouse hidden

--- scripts/components/mouse.tsx
+++ scripts/components/mouse.tsx
@@ -2,12 +2,18 @@ import classnames from "classnames";
 import * as React from "react";
 
 import s from "./mouse.scss";
+import { BehaviorSubject } from "rxjs";
 
 interface MouseState {
     isMouseDown: boolean;
 }
 
 export class Mouse extends React.Component<{}, MouseState> {
+    static set visible( value: boolean ) { Mouse.visibleEvent.next( value ); }
+    static get visible(): boolean { return Mouse.visibleEvent.value; }
+
+    private static visibleEvent = new BehaviorSubject<boolean>( false );
+
     private wrapperRef = React.createRef<HTMLDivElement>();
 
     state = {
@@ -27,6 +33,7 @@ export class Mouse extends React.Component<{}, MouseState> {
                     ref={ this.wrapperRef }>
             <div className={ classnames( s.center, s.element ) }></div>
             <div className={ classnames( s.centerFrame, s.element ) }></div>
+            <div className={ classnames( s.label, s.element ) }>Drag &amp; Click</div>
         </div>;
     }
 
